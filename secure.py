2dae94ace3079557bac038a5d7bd1fd2298e7e33
Eddie Vu <englishscily@gmail.com>
2023-04-07 11:58:18 -0700

    page 1 and dyk1 dyk1 WIP

--- secure.py
+++ secure.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 # In order to use "sessions",you need a "secret key".
 # This is something random you generate.  
 # For more info see: https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY
-"""
+
 app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.  
                                      #The value should be set on the server. 
                                      #To run locally, set in env.bat (env.sh on Macs) and include that file in gitignore so the secret key is not made public.
-"""
+
 @app.route('/')
 def renderMain():
     return render_template('home.html')
@@ -22,10 +22,22 @@ def startOver():
     session.clear() #clears variable values and creates a new session
     return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
 
-@app.route('/page1')
+@app.route('/page1', methods=['POST', 'GET'])
 def renderPage1():
+    if request.method == 'POST':
+        password_choice = request.form['password']
+        session['password_choice'] = password_choice
+        return redirect(url_for('renderDyk1'))
     return render_template('page1.html')
 
+    
+    
+    
+    
+    
+    
+    
+"""
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
     session["firstName"]=request.form['firstName']
@@ -36,6 +48,6 @@ def renderPage2():
 def renderPage3():
     session["favoriteColor"]=request.form['favoriteColor']
     return render_template('page3.html')
-    
+  """  
 if __name__=="__main__":
     app.run(debug=True)
