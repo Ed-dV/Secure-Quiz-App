f19eb8c574fcf67946509c25da0c70557ee4534b
Eddie Vu <129797363+Ed-dV@users.noreply.github.com>
2023-04-14 13:00:45 -0700

    DONE3

--- secure.py
+++ secure.py
@@ -9,25 +9,24 @@ app = Flask(__name__)
 
 app.secret_key = os.environ.get('SECRET_KEY')
 
-x = 0
 
-def count1():
-    count1 = x
+
+def count1(current_count):
     if session['password_choice'] == "password3":
-        count1 += 1
-    return count1
+        current_count += 1
+    return current_count
     
-def count2():
-    count1 = x
+def count2(current_count):
     if session['password_choice'] == "answer5":
-        count1 += 1
-    return count2
+        current_count += 1
+    return current_count
     
-def count3():
-    count1 = x
+    
+def count3(current_count):
     if session['password_choice'] == "p3_1":
-        count1 += 1
-    return count3
+        current_count += 1
+    return current_count
+    
 
 @app.route('/')
 def renderMain():
@@ -43,31 +42,49 @@ def renderPage1():
     if request.method == 'POST':
         password_choice = request.form['password']
         session['password_choice'] = password_choice
+        session['page1_completed'] = True  
+        return render_template('dyk1.html')
+    
+    if 'page1_completed' in session:  
         return render_template('dyk1.html')
     
     return render_template('page1.html')
 
+
 @app.route('/page2', methods=['POST', 'GET'])
 def renderPage2():
     if request.method == 'POST':
-        answer_choice = request.form['answer']
-        session['answer_choice'] = answer_choice
+        password_choice = request.form['answer']
+        session['password_choice'] = password_choice
+        session['page2_completed'] = True 
         return render_template('dyk2.html')
+    
+    if 'page2_completed' in session:  
+        return render_template('dyk2.html')
+    
     return render_template('page2.html')
 
 @app.route('/page3', methods=['POST', 'GET'])
 def renderPage3():
     if request.method == 'POST':
-        p3_choice = request.form['anw']
-        session['p3_choice'] = p3_choice
+        password_choice = request.form['anw']
+        session['password_choice'] = password_choice
+        session['page3_completed'] = True  
         return render_template('dyk3.html')
-    return render_template('page3.html')  
+    
+    if 'page3_completed' in session:  
+        return render_template('dyk3.html')
+    
+    return render_template('page3.html')
+
 
 @app.route('/qscore', methods=['POST', 'GET'])
 def renderScore():
-    score = count1()
-    return render_template('qscore.html', score=score)  
-   
+    score = 0
+    score = count1(score)
+    score = count2(score)
+    score = count3(score)
+    return render_template('qscore.html', score=score)
     
 if __name__ == "__main__":
     app.run(debug=False)
